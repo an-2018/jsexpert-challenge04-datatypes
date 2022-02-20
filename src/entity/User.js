94bc63e9ca4f2b48046e4a5a53d3f3e06c9d0c5f
anilson <anilsonmonteiro@gmail.com>
2022-02-19 23:00:56 -0100

    add users data struture and method add user string coersion method

--- src/entity/User.js
+++ src/entity/User.js
@@ -3,6 +3,14 @@ class User {
     this.id = id;
   }
   // TODO: Alguma manipulação a fazer considerando que o objeto gerado será constantemente escrito em tela?
+  [Symbol.toPrimitive](hint) {
+    if(hint === "string") {
+      return `User - id: ${this.id}`
+    }
+  }
+  toString() {
+    return `User - id: ${this.id}`
+  }
 }
 
 export default User;
--- src/entity/Users.js
+++ src/entity/Users.js
@@ -1,22 +1,31 @@
 import User from './User.js';
 
 // TODO: Criar um Symbol para a propriedade privada 'kUsers'
+const kUsers = Symbol('Users')
+const kData = Symbol('Data')
 class Users {
   constructor() {
     // TODO: inicializar a propriedade privada 'kData' como uma estrutura importante vista no curso
+    this[kData] = new Map()
   }
 
   add(userRaw) {
     const user = new User(userRaw);
     // TODO: inserir valor na estrutura escolhida.
+    this[kData].set(userRaw)
   }
 
   hasUsers() {
     // TODO: Como saber se tem informação dentro da estrutura escolhida?
-    return false;
+    return this[kData].size > 0;
   }
 
   // TODO: Me parece que o objeto gerado precisa ser iterável 🤔
+  *[Symbol.iterator]() {
+    for(const item of this[kData]){
+      yield item
+    }
+  }
 }
 
 export default Users;
