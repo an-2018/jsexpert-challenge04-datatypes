ece51d5517bf2d5dd2737db92b03e17089a7fd03
anilson <anilsonmonteiro@gmail.com>
2022-02-20 17:47:09 -0100

    fix crypto service listing implementation

--- src/service/CryptoService.js
+++ src/service/CryptoService.js
@@ -4,36 +4,36 @@ import CryptoRepository from '../repository/CryptoRepository.js';
 class CryptoService {
   constructor({ repository } = {}) {
     this.repository = repository || new CryptoRepository();
+    this.currentPage = 1
+    this.isLast = false
   }
   async *list() {
     // implementar generator que chama a repository fazendo a paginação
-    let isLast = false
-    let currentPage = 1
-    let pageLimit = 5
-    while (!isLast) {
-      const response = await this.repository.list(currentPage, pageLimit)
 
+    let limit = 5
+    while (!this.isLast) {
+
+      const response = await this.repository.list(this.currentPage, limit)
       const data = response.data
-      
-      const [first, nexturl, lasturl] = response.headers.link.split(',')
-      currentPage = this.extractNumber(nexturl)
-      let lastPage = this.extractNumber(lasturl)
-      
-      isLast = currentPage === lastPage
-      
+
+      if (this.hasNext(response.headers.link)) {
+        this.currentPage++
+      } else {
+        this.currentPage = 1
+      }
+
       yield data
     }
   }
 
-  extractNumber(str) {
-    /* link: '
-            <http://localhost:3001/crypto?_page=1&_limit=5>; rel="first", 
-            <http://localhost:3001/crypto?_page=2&_limit=5>; rel="next", 
-            <http://localhost:3001/crypto?_page=1000&_limit=5>; rel="last"',
-    */
-    return Number(str.match(/_page=?\d*/)[0].split('=')?.[1])
+  hasNext(str) {
+    if (!str) return false
+    let res = str.split(',').find(element => {
+      console.log(element)
+      return element.match(/<(.*?)>; rel="next"/)
+    });
+    return res ? true : false
   }
-
 }
 
 export default CryptoService;
